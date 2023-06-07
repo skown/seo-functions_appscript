13b45742ad17695beab79c0a9fbe59869bb3ac2f
adrian_skowron <adrian_skowron@MacBook-Pro-adrian-skowron.local>
2023-06-07 15:26:38 +0200

    Credentials change

--- related_searches.js
+++ related_searches.js
@@ -1,31 +1,33 @@
 /**
-* Gathering related searches from Google SERPs.
-* @constructor
-* @param {string} keyword - For which keyword do you want to check related searches.
-* @return The data results from Google SERPs.
-* @customfunction
-*/
+ * Gathering related searches from Google SERPs.
+ * @constructor
+ * @param {string} keyword - For which keyword do you want to check related searches.
+ * @return The data results from Google SERPs.
+ * @customfunction
+ */
 function related_searches(keyword) {
-	const url = "https://api.dataforseo.com/v3/serp/google/organic/live/advanced"
+  const url = "https://api.dataforseo.com/v3/serp/google/organic/live/advanced";
   const headers = {
-    'method': "POST",
-    'content-type': 'application/json',
-    'headers': {
-      "Authorization": 'Basic ' + Utilities.base64Encode('adrian.skowron@brainly.com' + ':' + '6a82d00b8f673110')
-      },
-      'muteHttpExceptions': true,
-      "payload": JSON.stringify([{'keyword': keyword, 'language_code': 'pt', 'location_code': 2076}])
-  }
+    method: "POST",
+    "content-type": "application/json",
+    headers: {
+      Authorization: "Basic " + Utilities.base64Encode("mail" + ":" + "pass"),
+    },
+    muteHttpExceptions: true,
+    payload: JSON.stringify([
+      { keyword: keyword, language_code: "pt", location_code: 2076 },
+    ]),
+  };
 
-  const response = UrlFetchApp.fetch(url, headers)
-  const content = JSON.parse(response.getContentText())
+  const response = UrlFetchApp.fetch(url, headers);
+  const content = JSON.parse(response.getContentText());
   //console.log(content['tasks'][0]['result'][0]['items'])
-  const results = content['tasks'][0]['result'][0]['items']
+  const results = content["tasks"][0]["result"][0]["items"];
 
   for (let i = 0; i < results.length; i++) {
-    let obj = results[i]
-    if (obj['type'] == 'related_searches') {
-      return obj['items']
+    let obj = results[i];
+    if (obj["type"] == "related_searches") {
+      return obj["items"];
     }
   }
 }
